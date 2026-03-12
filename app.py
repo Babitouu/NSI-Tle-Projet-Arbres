480758a7080b189da29c0c334c67f8d0168b90b8
Cédric VILLEMONAIS <cedric.villemonais@gmail.com>
2026-03-12 16:50:23 +0100

    Changement dossier + update python + HTML + CSS (sauf SVG) + début du JS

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder=".")
+app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
-def home():
-    tree_type_search = request.form["tree_type"] == "search" if request.form else False
-    return render_template('index.html',tree_type_search=tree_type_search)
+def tree():
+    tree_type_search = False
+    tree = None
+    if request.form:
+        if request.form["init"]:
+            tree = request.form["value"]
+            tree_type_search = request.form["type"] == "search"
+    return render_template('index.html',tree_type_search=tree_type_search,tree=tree)
 
 if __name__ == "__main__":
     app.run()

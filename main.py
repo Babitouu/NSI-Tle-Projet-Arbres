de04999acfe35b43cabeecf10fa6feaecd9c93e3
Cédric VILLEMONAIS <cedric.villemonais@gmail.com>
2026-03-13 23:07:02 +0100

    Fin de la transition de Flask à PyScript

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from pyscript import when, display, document
+from pyscript import web, when, display, document
 
-@when("click", "footer")
+input_values = web.page["#input_values"]
+input_type = web.page["#input_type"]
+
+search_div = web.page["#search_div"]
+browse_div = web.page["#browse_div"]
+
+@when("submit", "#form_values_type")
 def handler():
-    display("Button clicked!")
+    if "display_none" in search_div.classes:
+        if input_type.value == "search":
+            search_div.classes.remove("display_none")
+    if "display_none" in browse_div.classes:
+        if input_values.value != "":
+            browse_div.classes.remove("display_none")

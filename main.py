7c66337e5922b7a4e57a4cdf649a89a231c61136
Cédric VILLEMONAIS <cedric.villemonais@gmail.com>
2026-04-15 17:04:15 +0200

    Implémentation de la recherche

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Import des modules PyScript pour interagir avec la page web (DOM + évènements)
 from pyscript import web, when, display, document
 # Import des objets JavaScript pour afficher des logs et observer le redimensionnement du SVG
-from js import console, ResizeObserver, window
+from js import console, ResizeObserver, window, alert
 # Import de l'outil permettant de créer un proxy Python utilisable côté JavaScript
 from pyodide.ffi import create_proxy
 # Import du module d'expressions régulières
@@ -70,13 +70,15 @@ def submit_form_values_type():
             for elt in values:
                 tree.inserer(elt)
 
-            if "display_none" in browse_div.classes:
-                browse_div.classes.remove("display_none")
+
             if "display_none" in search_div.classes:
                 search_div.classes.remove("display_none")
 
             display_height.textContent = tree.hauteur()
             display_size.textContent = tree.taille()
+
+        if "display_none" in browse_div.classes:
+            browse_div.classes.remove("display_none")
 
         display_browse_list.textContent = ""
         display_browse_type.textContent = ""
@@ -111,7 +113,20 @@ observer.observe(document.getElementById("tree"))
 @when("submit", form_search)
 def submit_form_search():
     toolbox_menu()
-    console.log("Recherche demandée pour : ",input_search.value)
+    nodes = web.page.find(".tree_node")
+    texts = web.page.find(".node_text")
+    for i in range(len(nodes)):
+        if "tree_node_searched" in nodes[i].classes:
+            nodes[i].classes.remove("tree_node_searched")
+
+    value_clean = re.sub(r"[^0-9,.\-]", "", input_search.value) if input_search.value and input_search.value != "" and input_search.value != "-" else ""
+    if value_clean != "" and tree.rechercher(float(value_clean) if "." in value_clean else int(value_clean)):
+        for i in range(len(nodes)):
+            if texts[i].textContent == input_search.value:
+                nodes[i].classes.add("tree_node_searched")
+    else:
+        alert("Valeur non trouvée")
+
     # Réinitialisation du formulaire après traitement
     form_search.reset()
 
@@ -147,8 +162,8 @@ def generate_tree(type):
 
             nodes += f"""
                 <g transform="translate({x}, {y})">
-                    <circle r="22" fill="#0f172a" stroke="#22d3ee" stroke-width="3" />
-                    <text text-anchor="middle" dy="5" class="node-text">{arbre.noeud.v}</text>
+                    <circle class="tree_node" data-value="{arbre.noeud.v}" />
+                    <text dy="5" class="node_text">{arbre.noeud.v}</text>
                 </g>
             """
 
